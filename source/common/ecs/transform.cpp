d5602bd38023a30cd55e67652b921df8f0ce5741
ali bahr <alibahr313@gmail.com>
2025-03-15 10:28:15 +0200

    [feat] implement transformation matrix calculations in Transform class and vertex shader.

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -9,8 +9,11 @@ namespace our {
     // Remember that the order of transformations is: Scaling, Rotation then Translation
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
-        //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        glm::mat4 TransformationMatrix = glm::mat4(1.0f);
+        TransformationMatrix = TransformationMatrix *  glm::translate(glm::mat4(1.0f), position);
+        TransformationMatrix = TransformationMatrix * glm::yawPitchRoll(rotation.y, rotation.x, rotation.z)  ;
+        TransformationMatrix = TransformationMatrix* glm::scale(glm::mat4(1.0f), scale);
+        return TransformationMatrix;
     }
 
      // Deserializes the entity data and components from a json object
