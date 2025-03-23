1f503116cd4db71dfcddbdf4965086508187bd4e
ali bahr <alibahr313@gmail.com>
2025-03-23 04:24:37 +0200

    [Feat] Implement and test Forward Render System (req 9)

--- source/common/systems/forward-renderer.cpp
+++ source/common/systems/forward-renderer.cpp
@@ -22,7 +22,20 @@ namespace our {
             //TODO: (Req 10) Pick the correct pipeline state to draw the sky
             // Hints: the sky will be draw after the opaque objects so we would need depth testing but which depth funtion should we pick?
             // We will draw the sphere from the inside, so what options should we pick for the face culling.
-            PipelineState skyPipelineState{};
+            PipelineState skyPipelineState{
+                .faceCulling = {
+                        true,
+                        GL_FRONT,
+                        GL_CCW
+                },
+                .depthTesting{
+                    true,
+                    GL_LEQUAL
+                },
+                .blending{
+                        false
+                }
+            };
             
             // Load the sky texture (note that we don't need mipmaps since we want to avoid any unnecessary blurring while rendering the sky)
             std::string skyTextureFile = config.value<std::string>("sky", "");
@@ -135,21 +148,32 @@ namespace our {
 
         //TODO: (Req 9) Modify the following line such that "cameraForward" contains a vector pointing the camera forward direction
         // HINT: See how you wrote the CameraComponent::getViewMatrix, it should help you solve this one
-        glm::vec3 cameraForward = glm::vec3(0.0, 0.0, -1.0f);
+        glm::vec4 forwardInCameraSpace =  glm::vec4(0.0,0.0,-1.0f,0.0);
+        glm::vec3 cameraForward = glm::vec3(forwardInCameraSpace * camera->getOwner()->getLocalToWorldMatrix());
+
         std::sort(transparentCommands.begin(), transparentCommands.end(), [cameraForward](const RenderCommand& first, const RenderCommand& second){
             //TODO: (Req 9) Finish this function
-            // HINT: the following return should return true "first" should be drawn before "second". 
-            return false;
+            // HINT: the following return should return true "first" should be drawn before "second".
+            float distanceFirst  = glm::dot(cameraForward, first.center - cameraForward);
+            float distanceSecond  = glm::dot(cameraForward, second.center - cameraForward);
+            return distanceFirst > distanceSecond;
         });
 
+
         //TODO: (Req 9) Get the camera ViewProjection matrix and store it in VP
-        
+        glm::mat4 VP = camera->getProjectionMatrix(windowSize) * camera->getViewMatrix();
+
         //TODO: (Req 9) Set the OpenGL viewport using viewportStart and viewportSize
-        
+        glViewport(0.0f,0.0f, windowSize.x,windowSize.y);
+
+
         //TODO: (Req 9) Set the clear color to black and the clear depth to 1
-        
+        glClearDepth (1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
         //TODO: (Req 9) Set the color mask to true and the depth mask to true (to ensure the glClear will affect the framebuffer)
-        
+        glColorMask(true, true, true, true);
+        glDepthMask(true);
 
         // If there is a postprocess material, bind the framebuffer
         if(postprocessMaterial){
@@ -158,10 +182,18 @@ namespace our {
         }
 
         //TODO: (Req 9) Clear the color and depth buffers
-        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         //TODO: (Req 9) Draw all the opaque commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for(auto command: opaqueCommands){
+            command.material->shader->use();
+            command.material->shader->set("transform", VP * command.localToWorld);
+            command.material->setup();
+            command.mesh->draw();
+        }
+
+
         // If there is a sky material, draw the sky
         if(this->skyMaterial){
             //TODO: (Req 10) setup the sky material
@@ -185,7 +217,13 @@ namespace our {
         }
         //TODO: (Req 9) Draw all the transparent commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for (auto &command : transparentCommands)
+        {
+            command.material->shader->use();
+            command.material->shader->set("transform", VP * command.localToWorld);
+            command.material->setup();
+            command.mesh->draw();
+        }
 
         // If there is a postprocess material, apply postprocessing
         if(postprocessMaterial){
