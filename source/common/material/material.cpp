1aee169e087ac6815893cbf84435a8d4a56c0aca
Mostafa Rabie <mostafamohammed27.mm@gmail.com>
2025-04-22 22:26:42 +0200

    fix material

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -26,7 +26,7 @@ namespace our {
     // This function should call the setup of its parent and
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
-        //TODO: (Req 7) Write this function (DONE ✅)
+        //TODO: (Req 7) Write this function
         Material::setup();
         shader->set("tint", tint);
     }
@@ -42,8 +42,8 @@ namespace our {
     // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
-        //TODO: (Req 7) Write this function (DONE ✅)
-        TintedMaterial::setup();
+        //TODO: (Req 7) Write this function
+        Material::setup();
         shader->set("alphaThreshold", alphaThreshold);
         if (texture && sampler)
         {
